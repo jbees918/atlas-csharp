3cd294f4c0ccd78e53888894c738f0be5875076d
jbees918 <josh.beestoned@gmail.com>
2024-01-24 18:50:17 -0600

    generics queue task

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary> Class Queue </summary>
+	public class Queue<T>
+    {
+       
+	   /// <summary> Return type of queue </summary>
+		public Type CheckType()
+    	{
+			return typeof(T);
+		}
+
+		
+
+    }
