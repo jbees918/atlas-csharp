3ffd5b5cbde28550f792126eedecb7ad09d4b641
jbees918 <josh.beestoned@gmail.com>
2024-01-24 22:00:02 -0600

    fixing peek methods

--- csharp-generics/3-peek/queue.cs
+++ csharp-generics/3-peek/queue.cs
@@ -1,21 +1,39 @@
 ﻿using System;
 
-/// <summary> Represents a generic queue. </summary>
+/// <summary>
+/// Represents a generic queue.
+/// </summary>
+/// <typeparam name="T">The type of elements in queue.</typeparam>
 public class Queue<T>
 {
-    /// <summary> Represents a node in the queue. </summary>
+    /// <summary>
+/// Peeks at value of first node in queue without removing it.
+/// </summary>
+/// <returns> Value of first node in queue.</returns>
+    public T Peek()
+{
+    if (head == null)
+    {
+        Console.WriteLine("Queue is empty");
+        return default(T);
+    }
+    return head.value;
+}
+
+    /// <summary>
+    /// Represents a node in the queue.
+    /// </summary>
     public class Node
     {
-        private Node head, tail;
-        private int count;
-
         /// <summary> Gets or sets the value of the node. </summary>
         public T value { get; set; }
 
         /// <summary> Gets or sets the next node in the queue. </summary>
         public Node next { get; set; }
 
-        /// <summary> Initializes a new instance of the <see cref="Node"/> class. </summary>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Node"/> class.
+        /// </summary>
         /// <param name="value">The value of the node.</param>
         public Node(T value)
         {
@@ -27,11 +45,15 @@ public class Queue<T>
     private Node head, tail;
     private int count;
 
-    /// <summary> Returns the Queue's type. </summary>
+    /// <summary>
+    /// Returns the Queue's type.
+    /// </summary>
     /// <returns>The type of the Queue.</returns>
     public Type CheckType() => typeof(T);
 
-    /// <summary> Enqueue Method </summary>
+    /// <summary>
+    /// Enqueue Method.
+    /// </summary>
     /// <param name="value">The value to enqueue.</param>
     /// <returns>The value that was enqueued.</returns>
     public T Enqueue(T value)
@@ -48,7 +70,9 @@ public class Queue<T>
         return node.value;
     }
 
-    /// <summary> Dequeue Method </summary>
+    /// <summary>
+    /// Dequeue Method.
+    /// </summary>
     /// <returns>The value that was dequeued.</returns>
     public T Dequeue()
     {
@@ -63,19 +87,9 @@ public class Queue<T>
         return node.value;
     }
 
-    /// <summary> Peek Method </summary>
-    /// <returns>The value of the first node in the queue without removing it.</returns>
-    public T Peek()
-    {
-        if (head == null)
-        {
-            Console.WriteLine("Queue is empty");
-            return default(T);
-        }
-        return head.value;
-    }
-
-    /// <summary> Counts Nodes in Queue </summary>
+    /// <summary>
+    /// Counts Nodes in Queue.
+    /// </summary>
     /// <returns>The number of nodes in the queue.</returns>
     public int Count()
     {
