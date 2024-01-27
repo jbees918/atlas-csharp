02ae1cbf32ca9d814e0b1596b27aa7c61b1a67b4
jbees918 <josh.beestoned@gmail.com>
2024-01-27 16:07:49 -0600

    moved files

---
+++ csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,32 @@
+﻿public class Player
+{
+    private float maxHp;
+    private string name;
+    private float hp;
+
+    /// <summary>
+    /// Creates new Player.
+    /// </summary>
+    /// <param name="name">Player name.</param>
+    /// <param name="maxHp">Max health points of player. Must be greater than 0. Default is 100f.</param>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0)
+        {
+            maxHp = 100f;
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+
+        this.name = name;
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+    }
+
+    /// <summary>
+    /// Print player health.
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
