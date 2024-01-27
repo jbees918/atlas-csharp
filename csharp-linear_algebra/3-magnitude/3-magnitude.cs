5285ed07414265d7da45ae95853ec145fb8d9e08
jbees918 <josh.beestoned@gmail.com>
2024-01-27 00:00:14 -0600

    3-mag

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+/// <summary>
+/// Class to perform mathematical operations on vectors.
+/// </summary>
+public class VectorMath
+{
+    /// <summary>
+    /// Calculates magnitude of a given vector.
+    /// </summary>
+    /// <param name="vector">Vector whose magnitude needs to be calculated.</param>
+    /// <returns>The magnitude of the vector, or -1 if the vector is not 2D or 3D.</returns>
+    public static double Magnitude(double[] vector)
+    {
+        if (vector == null || (vector.Length != 2 && vector.Length != 3))
+        {
+            return -1;
+        }
+
+        double sumOfSquares = 0;
+        foreach (var component in vector)
+        {
+            sumOfSquares += component * component;
+        }
+
+        double magnitude = Math.Sqrt(sumOfSquares);
+        return Math.Round(magnitude, 2);
+    }
+}
