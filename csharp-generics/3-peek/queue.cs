a41beead6365f2b2de08bbc5f692d994db1ba8c1
jbees918 <josh.beestoned@gmail.com>
2024-01-28 15:11:38 -0600

    namespace issue

--- csharp-generics/3-peek/queue.cs
+++ csharp-generics/3-peek/queue.cs
@@ -1,13 +1,62 @@
-﻿// Peek method.
-public T Peek()
+﻿using System;
+
+namespace YourNamespace
 {
-    if (Head != null)
+    public class Queue<T>
     {
-        return Head.Value;
-    }
-    else
-    {
-        Console.WriteLine("Queue is empty");
-        return default(T);
+        // Nested Node class
+        public class Node
+        {
+            public T Value { get; set; }
+            public Node Next { get; set; }
+
+            public Node(T value)
+            {
+                Value = value;
+            }
+        }
+
+        // Properties
+        public Node Head { get; private set; }
+        public Node Tail { get; private set; }
+        public int Count { get; private set; }
+
+        // Enqueue method
+        public void Enqueue(T item)
+        {
+            var newNode = new Node(item);
+
+            if (Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Tail.Next = newNode;
+            }
+
+            Tail = newNode;
+            Count++;
+        }
+
+        // Peek method
+        public T Peek()
+        {
+            if (Head != null)
+            {
+                return Head.Value;
+            }
+            else
+            {
+                Console.WriteLine("Queue is empty");
+                return default(T);
+            }
+        }
+
+        // Count method
+        public int Count()
+        {
+            return Count;
+        }
     }
 }
