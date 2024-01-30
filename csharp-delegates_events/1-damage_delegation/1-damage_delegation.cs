44c4f9b4ea3d82e0be22c01d5fb4c6c9f99213af
jbees918 <josh.beestoned@gmail.com>
2024-01-29 18:08:06 -0600

    dmag

--- csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
+++ csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -1,30 +1,32 @@
-﻿using System;
+﻿﻿using System;
+
 
 /// <summary> Player class </summary>
 public class Player
 {
-    /// <summary> Player name </summary>
+    /// <summary> Player's name </summary>
     protected string name;
-    /// <summary> Player maxHp </summary>
-    protected float maxHp;
-    /// <summary> Player hp </summary>
+    /// <summary> Player's maxHp </summary>
     protected float hp;
+    protected float maxHp;
+    /// <summary> Player's hp </summary>
 
     /// <summary> Player Constructor </summary>
     public Player(string name="Player", float maxHp=100f)
     {
+        this.name = name;
         if( maxHp <= 0f){
             Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
             maxHp = 100f;
         }
-        this.name = name;
         this.maxHp = maxHp;
         this.hp = this.maxHp;
     }
 
-    /// <summary> PrintHealth Method </summary>
+    /// <summary>The PrintHealth Method </summary>
     public void PrintHealth()
     {
         Console.WriteLine("{0} has {1} / {2} health", name, hp, maxHp);
     }
+
 }
