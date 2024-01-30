284ff19cefe32a1b0d699f05952e489339e0c859
jbees918 <josh.beestoned@gmail.com>
2024-01-29 20:23:41 -0600

    de-q

--- csharp-generics/2-dequeue/queue.cs
+++ csharp-generics/2-dequeue/queue.cs
@@ -1,50 +1,28 @@
-﻿Based on 1-enqueue, add a new method Dequeue() to the class Queue<T> that removes the first node in the queue and returns its value.
+﻿using System;
 
-If the queue is empty, the method should write Queue is empty to the console and return the default value of the parameter’s type
-carrie@ubuntu:~/csharp-generics/2-dequeue$ cat 0-main.cs
-using System;
-
-class Program
+/// <summary> Queue class </summary>
+public class Queue<T>
 {
-    static void Main(string[] args)
+    // ...
+
+    /// <summary> Removes the first element from the Queue and returns its value </summary>
+    public T Dequeue()
     {
-        Queue<int> myIntQ = new Queue<int>();
+        if (Head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
 
-        myIntQ.Enqueue(100);
-        myIntQ.Enqueue(101);
-        Console.WriteLine("Number of nodes in queue: " + myIntQ.Count());
+        var temp = Head;
+        Head = Head.Next;
+        Count--;
 
-        Console.WriteLine("First value: " + myIntQ.Dequeue());
-        Console.WriteLine("Number of nodes in queue: " + myIntQ.Count());
+        if (Head == null)
+        {
+            Tail = null;
+        }
 
-        Console.WriteLine("----------");
-
-        Queue<float> myFloatQ = new Queue<float>();
-
-        myFloatQ.Enqueue(9.8f);
-        Console.WriteLine("Number of nodes in queue: " + myFloatQ.Count());
-
-        Console.WriteLine("First value: " + myFloatQ.Dequeue());
-
-        Console.WriteLine("Number of nodes in queue: " + myFloatQ.Count());
-
-        Console.WriteLine("First value: " + myFloatQ.Dequeue());
+        return temp.Value;
     }
 }
-carrie@ubuntu:~/csharp-generics/2-dequeue$
-carrie@ubuntu:~/csharp-generics/2-dequeue$ dotnet run
-Number of nodes in queue: 2
-First value: 100
-Number of nodes in queue: 1
-----------
-Number of nodes in queue: 1
-First value: 9.8
-Number of nodes in queue: 0
-Queue is empty
-First value: 0
-carrie@ubuntu:~/csharp-generics/2-dequeue$
-Repo:
-
-GitHub repository: atlas-csharp
-Directory: csharp-generics
-File: 2-dequeue/, 2-dequeue/2-dequeue.csproj, 2-dequeue/queue.cs
