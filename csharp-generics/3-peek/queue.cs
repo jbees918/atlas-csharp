515978c13f2af36e97bec6e7dbd800e3c935048a
jbees918 <josh.beestoned@gmail.com>
2024-01-30 15:38:46 -0600

    peek

--- csharp-generics/3-peek/queue.cs
+++ csharp-generics/3-peek/queue.cs
@@ -1,11 +1,65 @@
-﻿using System;
+﻿﻿using System;
 
 /// <summary> Queue class </summary>
 public class Queue<T>
 {
-    // ...
 
-    /// <summary> Peek Method </summary>
+    class Node
+    {
+        public T value;
+        public Node next;
+
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    Node head;
+    Node tail;
+    
+    int count;
+
+    /// <summary> Return Queue type </summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    /// <summary> Enqueue Method </summary>
+    public T Enqueue(T value)
+    {
+        Node node = new Node(value);
+        if (head == null)
+        {
+            head = node;
+            tail = node;
+        }
+        else
+        {
+            tail.next = node;
+            tail = node;
+        }
+        count++;
+        return node.value;
+    }
+    
+    /// <summary> Dequeue Method </summary>
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        Node node = head;
+        head = node.next;
+        count--;
+        return node.value;
+    }
+
+    ///<summary> Peek Method </summary>
     public T Peek()
     {
         if (head == null)
@@ -15,6 +69,19 @@ public class Queue<T>
         }
         return head.value;
     }
+    
+    ///<summary> Count Nodes in the Queue </summary>
+    public int Count()
+    {
+        int i = 0;
+        Node node = head;
+        while (node != null)
+        {
+            i++;
+            node = node.next;
+        }
+        count = i;
+        return count;
+    }
 
-    // ...
 }
