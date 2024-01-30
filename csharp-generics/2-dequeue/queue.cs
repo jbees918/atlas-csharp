f5437e5c5489609138a181e27a5a1dbdeb8d8091
jbees918 <josh.beestoned@gmail.com>
2024-01-30 06:19:05 -0600

    dequeue-2

--- csharp-generics/2-dequeue/queue.cs
+++ csharp-generics/2-dequeue/queue.cs
@@ -1,42 +1,50 @@
-﻿using System;
+﻿﻿using System;
 
 /// <summary> Queue class </summary>
 public class Queue<T>
 {
-    int count;
-
-    /// <summary> Returns Queue type </summary>
-    public Type CheckType() => typeof(T);
-
-    ///<summary> Count Nodes in the Queue </summary>
-    public int Count()
-    {
-        int i = 0;
-        Node node = head;
-        while (node != null)
-        {
-            i++;
-            node = node.next;
-        }
-        count = i;
-        return count;
-    }
 
     class Node
     {
         public T value;
         public Node next;
 
-        public Node(T val)
+        public Node(T value)
         {
-            value = val;
-            next = null;
+            this.value = value;
+            this.next = null;
         }
     }
 
     Node head;
     Node tail;
+    
+    int count;
 
+    /// <summary> Returns the Queue's type </summary>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    /// <summary> Enqueue Method </summary>
+    public T Enqueue(T value)
+    {
+        Node node = new Node(value);
+        if (head == null)
+        {
+            head = node;
+            tail = node;
+        }
+        else
+        {
+            tail.next = node;
+            tail = node;
+        }
+        count++;
+        return node.value;
+    }
+    
     /// <summary> Dequeue Method </summary>
     public T Dequeue()
     {
@@ -51,20 +59,18 @@ public class Queue<T>
         return node.value;
     }
 
-    /// <summary> Enqueue Method </summary>
-    public T Enqueue(T value)
+    ///<summary> Counts Nodes in Queue </summary>
+    public int Count()
     {
-        Node node = new Node(value);
-        if (head == null)
+        int i = 0;
+        Node node = head;
+        while (node != null)
         {
-            head = tail = node;
+            i++;
+            node = node.next;
         }
-        else
-        {
-            tail.next = node;
-            tail = node;
-        }
-        count++;
-        return node.value;
+        count = i;
+        return count;
     }
+
 }
