303997bdfacad3b9f55ee4e36ff86bdacd97be3a
jbees918 <josh.beestoned@gmail.com>
2024-01-30 04:23:30 -0600

    dequeue

--- csharp-generics/2-dequeue/queue.cs
+++ csharp-generics/2-dequeue/queue.cs
@@ -3,7 +3,39 @@
 /// <summary> Queue class </summary>
 public class Queue<T>
 {
-    // ...
+    int count;
+
+    /// <summary> Returns Queue type </summary>
+    public Type CheckType() => typeof(T);
+
+    ///<summary> Count Nodes in the Queue </summary>
+    public int Count()
+    {
+        int i = 0;
+        Node node = head;
+        while (node != null)
+        {
+            i++;
+            node = node.next;
+        }
+        count = i;
+        return count;
+    }
+
+    class Node
+    {
+        public T value;
+        public Node next;
+
+        public Node(T val)
+        {
+            value = val;
+            next = null;
+        }
+    }
+
+    Node head;
+    Node tail;
 
     /// <summary> Dequeue Method </summary>
     public T Dequeue()
@@ -19,5 +51,20 @@ public class Queue<T>
         return node.value;
     }
 
-    // ...
+    /// <summary> Enqueue Method </summary>
+    public T Enqueue(T value)
+    {
+        Node node = new Node(value);
+        if (head == null)
+        {
+            head = tail = node;
+        }
+        else
+        {
+            tail.next = node;
+            tail = node;
+        }
+        count++;
+        return node.value;
+    }
 }
