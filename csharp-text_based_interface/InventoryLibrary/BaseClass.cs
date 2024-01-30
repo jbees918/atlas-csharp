2102e86fc4233a84f6d925f8b6c647bddf0a0f89
jbees918 <josh.beestoned@gmail.com>
2024-01-29 21:44:59 -0600

    baseclass

---
+++ csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace InventoryLibrary
+{
+    /// <summary> 
+    /// BaseClass class 
+    /// </summary>
+    public class BaseClass
+    {
+        /// <summary> 
+        /// unique id for BaseClass 
+        /// </summary>
+        public string Id { get; }
+
+        /// <summary> 
+        /// Timestamp for when the BaseClass was created 
+        /// </summary>
+        public DateTime CreatedAt { get; }
+
+        /// <summary> 
+        /// Timestamp for when BaseClass was last updated 
+        /// </summary>
+        public DateTime UpdatedAt { get; }
+
+        /// <summary> 
+        /// Constructor for BaseClass 
+        /// </summary>
+        public BaseClass()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            DateTime now = DateTime.Now;
+            this.CreatedAt = now;
+            this.UpdatedAt = now;
+        }
+    }
+}
