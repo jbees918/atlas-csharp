c20c90e960ead3dc010fada66cde9b7e95c2aab0
jbees918 <josh.beestoned@gmail.com>
2024-01-31 14:09:27 -0600

    matmul ops

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -3,21 +3,6 @@
 /// <summary> class Matrix Math </summary>
 public static class MatrixMath
 {
-    /// <summary> Matrix Addition Method</summary>
-    public static double[,] Add(double[,] matrix1, double[,] matrix2)
-    {
-        if (matrix1.GetLength(0) != matrix2.GetLength(0) ||
-        matrix1.GetLength(1) != matrix2.GetLength(1) ||
-        matrix1.GetLength(0) < 2 || matrix1.GetLength(0) > 3 ||
-        matrix1.GetLength(1) < 2 || matrix1.GetLength(1) > 3)
-                return new double[,] {{-1}};
-
-        double[,] sum = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
-        for (int i = 0; i < matrix1.GetLength(0); i++)
-            for (int j = 0; j < matrix1.GetLength(1); j++)
-                sum[i, j] = matrix1[i, j] + matrix2[i, j];
-        return sum;
-    }
 
     /// <summary> Mat Sca Mul Method </summary>
     public static double[,] MultiplyScalar(double[,] matrix, double scalar)
@@ -31,6 +16,22 @@ public static class MatrixMath
             for (int j = 0; j < matrix.GetLength(1); j++)
                  result[i, j] = matrix[i, j] * scalar;
         return result;
+    }
+
+     /// <summary> Matrix Addition Method</summary>
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        if (matrix1.GetLength(0) != matrix2.GetLength(0) ||
+        matrix1.GetLength(1) != matrix2.GetLength(1) ||
+        matrix1.GetLength(0) < 2 || matrix1.GetLength(0) > 3 ||
+        matrix1.GetLength(1) < 2 || matrix1.GetLength(1) > 3)
+                return new double[,] {{-1}};
+
+        double[,] sum = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+            for (int j = 0; j < matrix1.GetLength(1); j++)
+                sum[i, j] = matrix1[i, j] + matrix2[i, j];
+        return sum;
     }
 
     ///<summary> Valiadate Matrix to 2D or 3D </summary>
