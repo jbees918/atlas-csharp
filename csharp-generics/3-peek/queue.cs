7e649049374b4259fc236a535420348d87548ed2
jbees918 <josh.beestoned@gmail.com>
2024-01-31 01:09:40 -0600

    peek comments and xml tag addition

--- csharp-generics/3-peek/queue.cs
+++ csharp-generics/3-peek/queue.cs
@@ -1,87 +1,121 @@
 ﻿﻿using System;
 
-/// <summary> Queue class </summary>
+/// <summary>
+/// Generic Queue implementation.
+/// </summary>
+/// <typeparam name="T">Type of elements in the queue.</typeparam>
 public class Queue<T>
 {
-
-    class Node
+    // Private Node class for internal use
+    private class Node
     {
-        public T value;
-        public Node next;
+        /// <summary>
+        /// Gets or sets the value of the node.
+        /// </summary>
+        public T Value { get; }
 
-        public Node(T value)
+        /// <summary>
+        /// Gets or sets the next node in the queue.
+        /// </summary>
+        public Node Next { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the Node class with the specified value.
+        /// </summary>
+        /// <param name="val">The value of the node.</param>
+        public Node(T val)
         {
-            this.value = value;
-            this.next = null;
+            Value = val;
+            Next = null;
         }
     }
 
-    Node head;
-    Node tail;
-    
-    int count;
+    // Private fields
+    private Node head = null;
+    private Node tail = null;
+    private int count = 0;
 
-    /// <summary> Return Queue type </summary>
-    public Type CheckType()
-    {
-        return typeof(T);
-    }
+    /// <summary>
+    /// Gets the number of nodes in the queue.
+    /// </summary>
+    public int Count => count;
 
-    /// <summary> Enqueue Method </summary>
-    public T Enqueue(T value)
+    /// <summary>
+    /// Adds a new element to the end of the queue.
+    /// </summary>
+    /// <param name="value">The value to enqueue.</param>
+    public void Enqueue(T value)
     {
-        Node node = new Node(value);
-        if (head == null)
+        Node newNode = new Node(value);
+
+        if (tail == null)
         {
-            head = node;
-            tail = node;
+            head = tail = newNode;
         }
         else
         {
-            tail.next = node;
-            tail = node;
+            tail.Next = newNode;
+            tail = newNode;
         }
+
         count++;
-        return node.value;
     }
-    
-    /// <summary> Dequeue Method </summary>
+
+    /// <summary>
+    /// Removes and returns the first element in the queue.
+    /// </summary>
+    /// <returns>The value of the dequeued element.</returns>
     public T Dequeue()
     {
         if (head == null)
         {
             Console.WriteLine("Queue is empty");
-            return default(T);
+            return default;
         }
-        Node node = head;
-        head = node.next;
+
+        T value = head.Value;
+        head = head.Next;
         count--;
-        return node.value;
+
+        if (head == null)
+        {
+            tail = null;
+        }
+
+        return value;
     }
 
-    ///<summary> Peek Method </summary>
+    /// <summary>
+    /// Returns the value of the first element in the queue without removing it.
+    /// </summary>
+    /// <returns>The value of the first element.</returns>
     public T Peek()
     {
         if (head == null)
         {
             Console.WriteLine("Queue is empty");
-            return default(T);
+            return default;
         }
-        return head.value;
-    }
-    
-    ///<summary> Count Nodes in the Queue </summary>
-    public int Count()
-    {
-        int i = 0;
-        Node node = head;
-        while (node != null)
-        {
-            i++;
-            node = node.next;
-        }
-        count = i;
-        return count;
+
+        return head.Value;
     }
 
+    /// <summary>
+    /// Returns the type of elements stored in the queue.
+    /// </summary>
+    /// <returns>The type of elements in the queue.</returns>
+    public Type CheckType() => typeof(T);
+
+    /// <summary>
+    /// Prints all elements in the queue.
+    /// </summary>
+    public void Print()
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+            Console.WriteLine(temp.Value);
+            temp = temp.Next;
+        }
+    }
 }
