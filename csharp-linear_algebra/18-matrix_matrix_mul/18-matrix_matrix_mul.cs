0dc88f1ec8552cb8016b4df15f64700a203bf00e
jbees918 <josh.beestoned@gmail.com>
2024-01-31 13:53:31 -0600

    mat sca

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -1,39 +1,58 @@
 ﻿using System;
 
-/// <summary>
-///  Class responsible for matrix mathematical operations
-/// </summary>
-public class MatrixMath
+/// <summary> class Matrix Math </summary>
+public static class MatrixMath
 {
-    /// <summary Method to multiply two matrices </summary>
+    /// <summary> Matrix Addition Method</summary>
+    public static double[,] Add(double[,] matrix1, double[,] matrix2)
+    {
+        if (matrix1.GetLength(0) != matrix2.GetLength(0) ||
+        matrix1.GetLength(1) != matrix2.GetLength(1) ||
+        matrix1.GetLength(0) < 2 || matrix1.GetLength(0) > 3 ||
+        matrix1.GetLength(1) < 2 || matrix1.GetLength(1) > 3)
+                return new double[,] {{-1}};
+
+        double[,] sum = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
+        for (int i = 0; i < matrix1.GetLength(0); i++)
+            for (int j = 0; j < matrix1.GetLength(1); j++)
+                sum[i, j] = matrix1[i, j] + matrix2[i, j];
+        return sum;
+    }
+
+    /// <summary> Mat Sca Mul Method </summary>
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar)
+    {
+        if(matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3 ||
+        matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3)
+            return new double[,] {{-1}};
+
+        double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
+                 result[i, j] = matrix[i, j] * scalar;
+        return result;
+    }
+
+    ///<summary> Valiadate Matrix to 2D or 3D </summary>
+    public static bool ValidateMatrix(double[,] matrix)
+    {
+        if (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3 ||
+        matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3)
+            return false;
+        return true;
+    }
+
+    ///<summary> Matrix Multip Method </summary>
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
-        // Check if matrices can be multiplied
         if (matrix1.GetLength(1) != matrix2.GetLength(0))
-        {
-            // If not, return a matrix with a single element (-1)
-            return new double[,] { { -1 } };
-        }
+            return new double[,] {{-1}};
 
-        // Create a matrix to store the result
         double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
-
-        // Perform matrix multiplication
-        for (int i = 0; i < result.GetLength(0); i++)
-        {
-            for (int j = 0; j < result.GetLength(1); j++)
-            {
-                result[i, j] = 0;
-
-                // Sum the products of corresponding elements
-                for (int k = 0; k < matrix1.GetLength(1); k++)
-                {
-                    result[i, j] += matrix1[i, k] * matrix2[k, j];
-                }
-            }
-        }
-
-        // Return the resulting matrix
+        for (int Row_A = 0; Row_A < matrix1.GetLength(0); Row_A++)
+            for (int Col_B = 0; Col_B < matrix2.GetLength(1); Col_B++)
+                for (int Col_A = 0; Col_A < matrix1.GetLength(1); Col_A++)
+                    result[Row_A, Col_B] += matrix1[Row_A, Col_A] * matrix2[Col_A, Col_B];
         return result;
     }
 }
